a334591cb0f6ba57cb99758a18f3f6d51eb61a50
Abdur Rahim <nishanrahman1994@gmail.com>
2022-06-10 20:50:34 +0600

    Initial Commit

---
+++ src/Translator.php
@@ -0,0 +1,18 @@
+<?php
+namespace ArNishan\BanglaConverter;
+use ArNishan\BanglaConverter\Traits\AvroToBijoy;
+use ArNishan\BanglaConverter\Traits\BijoyToAvro;
+use App\Http\Controllers\Controller;
+
+class Translator extends Controller{
+    use AvroToBijoy,BijoyToAvro;
+    public function unicodeToBijoy($unicode){
+        $bijoy = $this->u2b($unicode);
+        return $bijoy;
+    }
+    public function bijoyToUnicode($bijoy){
+        $unicode = $this->b2u($bijoy);
+        return $unicode;
+    }
+
+}
---
+++ src/TranslatorServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace ArNishan\BanglaConverter;
+use Illuminate\Support\ServiceProvider;
+
+class TranslatorServiceProvider extends ServiceProvider{
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+}
